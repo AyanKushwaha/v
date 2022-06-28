9bf9093bf673db5645cc8f512cae56b76045c82d
jolly <Jolly@sas.dk>
2022-06-28 09:36:04 +0000

    SKCMS-3017:Modified code

--- lib/python/salary/wfs/wfs_report.py
+++ lib/python/salary/wfs/wfs_report.py
@@ -27,6 +27,7 @@ ARCHIVE_PATH = "/samba-share/reports/SALARY_WFS/"
 RELEASE_PATH = '/opt/Carmen/CARMTMP/ftp/out/SALARY_SEIP/'
 seq_salary_rec_wfs = 'seq_salary_rec_wfs'
 seq_salary_run_wfs = 'seq_salary_run_wfs'
+crew_exclusion_list = ['92742','92589','92462']
 
 logging.basicConfig()
 log = logging.getLogger('wfs_report')
@@ -102,6 +103,9 @@ class WFSReport():
             for crew_id in crew_ids:
                 if crew_has_retired_at_date(crew_id, curr_date):
                     log.info('NORDLYS: Skipping retired crew {c} on {d}'.format(c=crew_id, d = curr_date))
+                    crew_ids.remove(crew_id)
+                if crew_excluded(crew_id, curr_date):
+                    log.info('NORDLYS: Skipping the excluded SAS Link crew {c}'.format(c=crew_id))
                     crew_ids.remove(crew_id)
             log.debug('NORDLYS: Generating {0} report for crew {1}'.format(self.type, crew_ids))
 
@@ -436,7 +440,6 @@ def planninggroup_from_id(crew_id, dt):
         )[0]
     return planninggroup
 
-crew_exclusion_list = ['92742','92589','92462']
 
 def crew_excluded(crew_id, curr_date):
     if extperkey_from_id(crew_id,curr_date) in crew_exclusion_list:
