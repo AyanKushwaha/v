994adb78605b61e1d0ccaddb45c1ad23fb2e9d41
jolly <Jolly@sas.dk>
2022-07-04 12:28:28 +0000

    SKCMS-2998:Link CC SCC course set up

---
+++ data/migration/r27_sas2207/skcms-2998.py
@@ -0,0 +1,24 @@
+"""
+SKCMS-2998: Link CC SCC course set up
+"""
+
+
+import adhoc.fixrunner as fixrunner
+from AbsTime import AbsTime
+
+__version__ = '2022-07-05'
+
+
+@fixrunner.once
+@fixrunner.run
+def fixit(dc, *a, **k):
+    ops = list()
+    
+    ops.append(fixrunner.createOp('training_log_set', 'N', {'typ': 'LINE FLIGHT SCC', 'grp': 'FLT TRAINING', 'si': 'Added in SKCMS-2998'}))
+
+    return ops
+
+
+fixit.program = 'skcms-2998.py (%s)' % __version__
+if __name__ == '__main__':
+    fixit()
