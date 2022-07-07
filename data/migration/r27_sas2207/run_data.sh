8d0c311d33540217603fbed671ecdbfe4968d6c3
Palak Mattu <palak.mattu@sas.dk>
2022-07-07 17:29:06 +0000

    SKCMS-2975: Script moved to R27_sas2207

--- data/migration/r27_sas2207/run_data.sh
+++ data/migration/r27_sas2207/run_data.sh
@@ -36,8 +36,8 @@ echo " - Processing migration tasks"
 # echo "  * SKCMS-1234: Some description"
 # python $CARMUSR/data/migration/$RELEASE/skcms-1234.py 2>&1 | tee $LOG_DIR/skcms-1234.log
 
-
-
+echo "  * SKCMS-2975: Add LC AP-POS to table training_log_set"
+python $CARMUSR/data/migration/$RELEASE/skcms-2975.py 2>&1 | tee $LOG_DIR/skcms-2975.log
 
 # Uncomment this in case dave filters shall be modified, remember to update JIRA number
 #echo "  * SKCMS-XXXX: Importing crc/etable/dave_filter/*.etab into database"
