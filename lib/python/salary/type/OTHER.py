cce983e38729b59e50d9c2e44a5184620f2762ca
Shiva Shankar Raju(admin) <tcsssradm@cweudcmssm01.flysas.com>
2022-12-06 13:10:56 +0000

    EC: Changed instructor pay at A2NX ETOPS flights

--- lib/python/salary/type/OTHER.py
+++ lib/python/salary/type/OTHER.py
@@ -46,6 +46,8 @@ class InstructorIterator(RaveIterator):
             'sim': 'salary.%inst_sim%',
             'sim_skill_bd': 'salary.%inst_sim_skill_brief_debrief%',
             'new_hire_follow_up_act': 'salary.%inst_new_hire_follow_up_act%',
+            'etops_lifus_act': 'salary.%inst_etops_lifus_act%',
+            'etops_lc_act': 'salary.%inst_etops_lc_act%',
         }
         RaveIterator.__init__(self, iterator, fields)
 
@@ -293,6 +295,12 @@ class OtherTransactionsRun(GenericRun):
     def INST_LIFUS_ACT(self, rec):
         #return self.times100(rec.lifus_act)
         return self.makeDayList(rec, self.hours100(rec.lifus_act))
+    def INST_ETOPS_LIFUS_ACT(self, rec):
+        #return self.times100(rec.lifus_act)
+        return self.makeDayList(rec, self.hours100(rec.etops_lifus_act))
+    def INST_ETOPS_LC_ACT(self, rec):
+        #return self.times100(rec.lifus_act)
+        return self.makeDayList(rec, self.hours100(rec.etops_lc_act))
 
     def INST_CRM(self, rec):
         return self.makeDayList(rec, self.hours100(rec.crm))
