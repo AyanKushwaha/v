a0afdd687ca92e0853b27ea86220c898ee6af99a
TCSKUYADM <tcskuyadm@cweudcmsin01.flysas.com>
2024-03-06 16:13:53 +0000

    Checking conflict with 2nd branch

--- lib/python/salary/wfs/wfs_time_entry_report.py
+++ lib/python/salary/wfs/wfs_time_entry_report.py
@@ -24,7 +24,7 @@ from salary.wfs.wfs_config import PaycodeHandler
 import time
 
 HEADERS = ('EMPLOYEE_ID', 'PAY_CODE', 'WORK_DT', 'HOURS', 'DAYS_OFF')
-EXCLUDED_RANKS=['AA']
+EXCLUDED_RANKS=['A_A']
 
 logging.basicConfig()
 log = logging.getLogger('wfs_time_entry')
